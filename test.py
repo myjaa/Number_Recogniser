de069df1d33a7e772e1c89f79db1810332d718be
Mohammad yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-05-20 05:12:28 +0530

    completed model accuracy: approx 99% predictions accuracy via GUI- 2/5

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 import tensorflow as tf
 import cv2
+import numpy as np
 
 model=tf.keras.models.load_model('mnist_classifier.h5')
+
+#reading the image
+img=cv2.imread(r'C:\Users\yusuf\Downloads\my projects\Digit_recogniser\custom test\test.png')
+
+# changing the color channel
+img_predict=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img_predict=cv2.resize(img_predict,(28,28))
+img_predict=np.reshape(img_predict,(1,28,28,1))
+print(img_predict.shape)
+
+print(np.argmax(model.predict(img_predict)))
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
